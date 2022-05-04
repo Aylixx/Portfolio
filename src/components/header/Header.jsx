60a451b624bce0643ed2be656e62dad1dfa7c36f
unknown <cxaylix@gmail.com>
2022-05-04 16:21:48 -0600

    updated title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,9 @@ export default function Header() {
         <header>
             <div className="container header__container">
                 <h5>Hello World</h5>
-                <h1>Colton Aylix</h1>
+                <div className='header__title-gradient'>
+                    <h1>Colton Aylix</h1>
+                </div>
                 <h4 className="text-light">Fullstack Developer</h4>
 
                 <CTA />
